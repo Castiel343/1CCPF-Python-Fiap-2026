b911dbb13bfd2d2398e7ee20d36dfe9dcff15836
Castiel343 <olavodadariovb@gmail.com>
2026-03-17 11:43:54 -0300

    aula 2

---
+++ aula02-variaveis-operadores/Desafio.py
@@ -0,0 +1,12 @@
+
+# Atividade 1
+
+nome = ("Olavo")
+print (f"Boas vindas {nome}")
+
+#Variavel
+aniversario = 1/10/2007
+
+# Atividade 2
+
+aniversario = input("Digite sua data de nascimento")
---
+++ aula02-variaveis-operadores/aula02a-variaveis.py
@@ -0,0 +1,42 @@
+print ("ola mundo")
+print (7 + 4)
+print ("7 + 4")
+print ("7" + "4") # CONCATENAÇÃO DE STRINGS
+
+# Comentários de 1 linha
+'''
+comentarios
+de
+multiplas
+linhas
+'''
+
+# VARIÁVEIS
+nome = "Olavo"
+idade = "18"
+peso = "75.5"
+
+print (nome, idade, peso)
+print (f"Oii, {nome}!")
+
+# Input -- SIMULA FORMS NO CMD
+nome = input ("Digite seu nome:")
+idade = int(input) ("Digite sua idade:")
+peso = input ("Digite seu peso:")
+
+print (nome, idade, peso)
+
+# Atividade
+
+print (f"Boas vindas {nome}")
+
+#Variavel
+aniversario = 1/10/2007
+
+# Atividade 2
+
+aniversario = input("Digite sua data de nascimento")
+
+# atividade 3
+
+print (7 + 3)
